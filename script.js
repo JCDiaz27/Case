0bc7f928c758cf2636db715d108c62b10d181a56
John Carlo Diaz <59531206+JCDiaz27@users.noreply.github.com>
2025-08-13 12:00:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function showProjectDetails(projectId) {
+  // Add project detail modal functionality
+  alert("Project details functionality - implement modal here");
+}
